c7e6a8b32daa67d347a26eb31bc0b81560d22c73
Carole1998 <brrcmnfam24>
2022-04-25 17:03:52 +0200

    FizzBuzz

--- src/main/java/StringX.java
+++ src/main/java/StringX.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+import java.util.Arrays;
+
 public class StringX {
     public String stringX(String str) {
         String result = "";
@@ -20,7 +30,24 @@ public class StringX {
                 count++;
             }
         }
-
         return count;
+    }
+
+    public String[] fizzBuzz(int start, int end) {
+        String[] result = new String[end - start];
+        int arrayIndex = 0;
+        for (int index = start; index < end; index++) {
+            if (index % 3 ==0) {
+                result[arrayIndex] = "Fizz";
+            } else if (index % 5 ==0) {
+                result[arrayIndex] = "Buzz";
+            } else if (index % 15 ==0) {
+                result[arrayIndex] = "FizzBuzz";
+            } else {
+                result[arrayIndex] = String.valueOf(index);
+            }
+            arrayIndex++;
+        }
+        return result;
     }
 }
